14ee2e0fe0bd3e337f1c946882721f3d5d9209b1
yanresende <83774024+yanresende@users.noreply.github.com>
2023-01-17 09:43:11 -0300

    Exit option
    Included in the system the exit option for the user

--- Main.c
+++ Main.c
@@ -128,6 +128,7 @@ int main()
     printf("\t1 - Registrar nomes\n");
     printf("\t2 - Consultar nomes\n");
     printf("\t3 - Deletar nomes\n");
+    printf("\t4 - Sair do sistema\n");
     printf("Opção: ");
 
     scanf("%d", &opcao); //armazena a escolha do usuario
@@ -151,6 +152,11 @@ int main()
     deletar();
     system("cls");
     break;
+
+    case 4:
+        printf("Obrigado por utilizar o sistema!\n");
+        return 0;
+        break;
 
     default:
         printf("Essa opção não está disponivel!\n");
